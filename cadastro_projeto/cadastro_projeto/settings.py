2ce81eb407b0f5f8cfb5db1a96f3ba653de75718
Rodrigo <rodrigocesar12309@gmail.com>
2026-06-08 12:44:26 -0300

    deploy

--- cadastro_projeto/cadastro_projeto/settings.py
+++ cadastro_projeto/cadastro_projeto/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cadastro_projeto.urls'
@@ -124,3 +126,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
